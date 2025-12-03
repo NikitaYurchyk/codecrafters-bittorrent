7df39a63962fd07a6b5c774adde7c51aa500b273
Nikita <ny.yurchyk@gmail.com>
2025-12-03 03:26:37 +0100

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,20 +6,20 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+//    System.err.println("Logs from your program will appear here!");
     
     String command = args[0];
     if("decode".equals(command)) {
       //  TODO: Uncomment the code below to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
+        String bencodedValue = args[1];
+        String decoded;
+        try {
+          decoded = decodeBencode(bencodedValue);
+        } catch(RuntimeException e) {
+          System.out.println(e.getMessage());
+          return;
+        }
+        System.out.println(gson.toJson(decoded));
 
     } else {
       System.out.println("Unknown command: " + command);
